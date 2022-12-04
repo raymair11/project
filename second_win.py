f75603d22ffbc3c6c0a7e67abb7a07145c1eac4f
raymair11 <104037649+raymair11@users.noreply.github.com>
2022-12-04 13:26:58 +0300

    Add files via upload

--- second_win.py
+++ second_win.py
@@ -2,6 +2,8 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit
 from instr import *
 from final_win import *
+
+
 class SecondWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -9,10 +11,12 @@ class SecondWin(QWidget):
         self.unit()
         self.connects()
         self.show()
+
     def appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
+
     def unit(self):
         self.name = QLabel(name)
         self.line1 = QLineEdit('Ф.И.О')
@@ -28,45 +32,42 @@ class SecondWin(QWidget):
         self.line5 = QLineEdit('0')
         self.t = QLabel('hhhhhhh')
         self.buttonFinish = QPushButton('Отправить результаты')
-        self.button1 = QPushButton('Начать111')
-        self.button2 = QPushButton('Начать')
-        self.button3 = QPushButton('Начать')
-        self.button4 = QPushButton('Начать')
-        self.lay.addWidget(self.name,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.line1,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.age,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.line2,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.test1,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.button1,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.line3,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.test2,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.button2,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.test3,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.button4,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.line4,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.line5,alignment = Qt.AlignLeft)
-        self.lay.addWidget(self.t,alignment = Qt.AlignRight)
-        self.lay.addWidget(self.buttonFinish,alignment = Qt.AlignCenter)
+        self.button1 = QPushButton('Начать тест 1')
+        self.button2 = QPushButton('Начать тест 2')
+        self.button3 = QPushButton('Начать тест 3')
+        self.lay.addWidget(self.name, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.line1, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.age, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.line2, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.test1, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.button1, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.line3, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.test2, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.button2, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.test3, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.button3, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.line4, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.line5, alignment=Qt.AlignLeft)
+        self.lay.addWidget(self.t, alignment=Qt.AlignRight)
+        self.lay.addWidget(self.buttonFinish, alignment=Qt.AlignCenter)
 
         self.setLayout(self.lay)
+
     def connects(self):
         self.button1.clicked.connect(self.timer)
         self.buttonFinish.clicked.connect(self.next_click)
-        
+
     def next_click(self):
         self.tw = Finish()
         self.hide()
+
     def timer(self):
-        
+
         from time import sleep
-        for i in range(0,60,-1):
+        for i in range(0, 60, -1):
             self.t.setText(str(i))
             sleep(1)
-            
-            
 
 
 # app = QApplication([])
 # app.exec_()
-
-
